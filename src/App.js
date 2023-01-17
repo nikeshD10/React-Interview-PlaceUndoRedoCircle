7589e66ef2aa6dd6e6a348a405c31d1f74f97367
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-01-17 18:36:27 +0000

    Finally solved the question

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [points, setPoints] = useState([]);
+  const [undoPoints, setUndoPoints] = useState([]);
+
+  // Handling Undo
+  const handleUndo = () => {
+    const poppedPoints = points.pop();
+    setPoints([...points]);
+    setUndoPoints((undoPoints) => [...undoPoints, poppedPoints]);
+  };
+
+  // Handling Redo
+  const handleRedo = () => {
+    const redoPoints = undoPoints.pop();
+    setPoints((points) => [...points, redoPoints]);
+  };
+
+  // Handling Place Circle
+  const handlePlaceCircle = (event) => {
+    const { clientX, clientY } = event;
+    setPoints((points) => [...points, { x: clientX, y: clientY }]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button disabled={points.length === 0} onClick={handleUndo}>
+        Undo
+      </button>
+      <button disabled={undoPoints.length === 0} onClick={handleRedo}>
+        Redo
+      </button>
+      <div className="App" onClick={handlePlaceCircle}>
+        {points.map((point, index) => (
+          <div
+            key={index}
+            className="point"
+            style={{ left: point.x - 9 + "px", top: point.y - 8 + "px" }}
+          ></div>
+        ))}
+      </div>
+    </>
   );
 }
 
